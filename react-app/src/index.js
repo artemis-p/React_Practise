95c9fbcc22b53371842557bfaac1438f47eedead
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-09-23 14:57:50 +0100

    Created a react app that prints hello world

---
+++ react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+// ('react -> is the module and React is the object we are importing from this module')
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello World</h1>;
+ReactDOM.render(element, document.getElementById("root"));
